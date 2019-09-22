0c04be1651c74f2b4a02628b4da34b7112e1df06
Minchao Chen <minchao.chen@ericsson.com>
2019-09-23 07:06:23 +0800

    Add parsing definition and abbr

--- utils/jsonGeneration.py
+++ utils/jsonGeneration.py
@@ -7,7 +7,7 @@ def get_title_info(catelog):
     title_pre = catelog.split()[0]
     key = re.sub(title_pre, '', catelog)
     key = re.sub(r'\d+', '', key).strip()
-    print(title_level, title_pre, key)
+    #print(title_level, title_pre, key)
     return (key, title_pre, title_level)
 
 def get_desc_info(title_infos, content):
@@ -26,8 +26,9 @@ def get_desc_info(title_infos, content):
         # get the start desc line num
         for j, line in enumerate(content[end_num:]):
             if (line.find(title_cur[0]) != -1 and line.find(title_cur[1]) != -1 and not line.split()[-1].isdigit()):
-                print(line.split(title_cur[1])[1].strip())
-                print(title_cur[0])
+                pass
+                #print(line.split(title_cur[1])[1].strip())
+                #print(title_cur[0])
             if(line.find(title_cur[0]) != -1 and line.find(title_cur[1]) != -1
                     and line.split(title_cur[1])[1].strip() == title_cur[0] and not line.split()[-1].isdigit()):
                 start_num = end_num + j
@@ -38,15 +39,16 @@ def get_desc_info(title_infos, content):
             if (line.find(title_next[0]) != -1 and line.find(title_next[1]) != -1
                     and title_next[0] != "Annex A (informative)"
                     and not line.split()[-1].isdigit()):
-                print(line.split(title_next[1])[1].strip())
-                print(title_next[0])
+                pass
+                #print(line.split(title_next[1])[1].strip())
+                #print(title_next[0])
             if(line.find(title_next[0]) != -1 and line.find(title_next[1]) != -1
                     and title_next[0] != "Annex A (informative)" and line.split(title_next[1])[1].strip() == title_next[0]
                     and not line.split()[-1].isdigit()):
                 end_num = start_num + j
                 break;
 
-        print(title_cur[1], start_num, end_num)
+        #print(title_cur[1], start_num, end_num)
         desc_infos.append(''.join(content[start_num : end_num]))
     return desc_infos
   
@@ -57,7 +59,7 @@ def create_spec_dict_list(title_infos, desc_infos):
         spec_dict = {}
         spec_dict['key'] = title_info[0] 
         spec_dict['numbering'] = title_info[1]
-        spec_dict['title_levle'] = title_info[2]
+        spec_dict['title_level'] = title_info[2]
         spec_dict['desc'] = desc_infos[i]
         spec_dict_list.append(spec_dict)
     return spec_dict_list
@@ -66,12 +68,75 @@ def save_json_file(file_name, dict_list):
     with open(file_name, 'w') as f:
         json.dump(dict_list, f)
 
+def get_def_key(def_input):
+    def_key_list = []
+    for line in def_input['desc'].split('\n'):
+        if(line.find(':') != -1):
+            def_key_list.append(line.split(':')[0])
+            print(line.split(':')[0])
+    return def_key_list
+
+def populateDefList(def_input):
+    def_key_list = get_def_key(def_input)
+    spec_def_list = []
+
+    for i, def_key in enumerate(def_key_list):
+        def_key_next = ''
+        if(i != len(def_key_list)-1):
+            def_key_next = def_key_list[i+1]
+
+        start = def_input['desc'].find(def_key+':') + len(def_key) + 1
+        if def_key_next != '':
+            end = def_input['desc'].find(def_key_next+':')
+        else:
+            end = len(def_input['desc'])
+
+        spec_defination = {}
+        spec_defination['key'] = def_key
+        spec_defination['desc'] = ''.join(def_input['desc'][start:end]).strip()
+        print(spec_defination['key'])
+        print(spec_defination['desc'])
+        spec_def_list.append(spec_defination)
+    return spec_def_list
+
+def populateAbbrList(abbr_input):
+    print("populateAbbrList")
+    abbr_input_start = abbr_input['desc'].split(".\n\n")[1]
+    abbr_def_list = []
+    for line in abbr_input_start.split('\n'):
+        line = line.strip()
+        print(line)
+        if(len(line.split()) > 1):
+            abbr_def = {}
+            abbr_def['key'] = line.split()[0]
+            abbr_def['desc'] = line[len(abbr_def['key']):].strip()
+            abbr_def_list.append(abbr_def)
+            print(abbr_def['key'])
+            print(abbr_def['desc'])
+        else:
+            print(len(line.split()))
+    return abbr_def_list
+
+def update_spec_list(def_index, abbr_index, def_list, abbr_list, spec_dict_list):
+    del spec_dict_list[def_index]
+    del spec_dict_list[abbr_index-1]
+
+    for def_line in def_list:
+        spec_dict_list.append(def_line)
+
+    for abbr_line in abbr_list:
+        spec_dict_list.append(abbr_line)
+
 def main(input, output):
     spec_dict_list = []
     title_infos = []
     desc_infos = []
     title_index = []
     content = {}
+    def_list = []
+    abbr_list = []
+    def_index = 0
+    abbr_index = 0
 
     with open(input, 'r') as f:
         content = f.readlines()
@@ -89,7 +154,16 @@ def main(input, output):
 
     desc_infos = get_desc_info(title_infos, content)
     spec_dict_list = create_spec_dict_list(title_infos, desc_infos)
-       
+
+    for i,spec_dict in enumerate(spec_dict_list):
+        if spec_dict['key'] == "Definitions":
+            def_list = populateDefList(spec_dict)
+            def_index = i
+        if spec_dict['key'] == "Abbreviations":
+            abbr_list = populateAbbrList(spec_dict)
+            abbr_index = i
+
+    update_spec_list(def_index, abbr_index, def_list, abbr_list, spec_dict_list)
     save_json_file(output, spec_dict_list)
 
 if __name__ == "__main__":
